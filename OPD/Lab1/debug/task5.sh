9b99a80d68c8b74bf7e2b9b6392e77983148460c
dxunvrs <r.shulai@yandex.ru>
2025-09-15 11:05:58 +0300

    opd-lab1-15.09morning

---
+++ OPD/Lab1/debug/task5.sh
@@ -0,0 +1,27 @@
+# для лабы ~/lab0
+cd ~/Projects/OPD/Lab1/lab0
+
+rm -r boldore5 
+
+rm -r camerupt0/carvanha 
+
+rm -r camerupt0/sphealduckle* 
+# ошибка: no matches found: camerupt0/sphealduckle*
+# выдадим camerupt0 право на чтение
+chmod u+r camerupt0
+rm -r camerupt0/sphealduckle* 
+
+rm -r camerupt0/sphealboldo* 
+
+rm -r pichu9
+# файл pichu9/snorlax защищен от записи и не удалился
+# выдадим ему право на запись
+chmod u+w pichu9/snorlax
+rm -r pichu9
+
+rm -r camerupt0/toxicroak
+# camerupt0/toxicroak защищен от записи, также как и файл camerupt0/toxicroak/ducklett1
+# выдадим им право на запись
+chmod u+w camerupt0/toxicroak
+chmod u+w camerupt0/toxicroak/ducklett1
+rm -r camerupt0/toxicroak
