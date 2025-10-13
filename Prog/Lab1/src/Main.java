a6120249d244cc4d7ddafec6c26936a5709a40c2
dxunvrs <r.shulai@yandex.ru>
2025-10-13 11:37:41 +0300

    Mb done?

--- Prog/Lab1/src/Main.java
+++ Prog/Lab1/src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        long[] l = new long[9];
-        float[] x = new float[13];
-        double[][] l1 = new double[9][13];
-
-        l = setEvenNumbers(l);
-        x = setRandomNumbers(x);
-        l1 = setMatrix(l1, l, x);
+        long[] l = setEvenNumbers(new long[9]);
+        float[] x = setRandomNumbers(new float[13]);
+        double[][] l1 = setMatrix(new double[9][13], l, x);
 
         showMatrix(l1);
     }
 
     public static long[] setEvenNumbers(long[] array) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = (i + 3) * 2;
+            array[i] = (i + 3) * 2L;
         }
         return array;
     }
@@ -29,15 +25,12 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 switch((int)l[i]) {
-                    case 16:
+                    case 16 ->
                         array[i][j] = Math.atan(1/(Math.pow(Math.exp(Math.pow(2*Math.pow(Math.cos(x[j]),2),Math.E)),Math.pow(((2/3+x[j])/0.25),x[j]))));
-                        break;
-                    case 6,8,10,20:
+                    case 6,8,10,20 ->
                         array[i][j] = Math.cos(Math.cbrt(Math.atan((x[j]+2)/14)));
-                        break;
-                    default:
+                    default ->
                         array[i][j] = Math.asin(Math.sin(Math.pow(Math.pow(x[j],(4-x[j])/x[j]), Math.log(Math.exp(x[j])))));
-                        break;
                 }
             }
         }
@@ -54,5 +47,3 @@ public class Main {
         }
     }
 }
-
-
