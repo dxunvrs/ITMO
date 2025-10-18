70523a05df0728fe4448074b4c14e0d9e61fefd7
dxunvrs <r.shulai@yandex.ru>
2025-10-18 18:56:23 +0300

    1-2 tasks

--- Inf/Lab3/Informatics_Lab3_Task2.py
+++ Inf/Lab3/Informatics_Lab3_Task2.py
@@ -6,20 +6,27 @@
 
 import re 
 
-email1 = "students.spam@yandex.ru"
-email2 = "example@example"
-email3 = "example@example.com"
 
-pattern = r"[a-zA-Z0-9._]+@([a-zA-Z]+\.[a-zA-Z]+)$"
+tests = ["students.spam@yandex.ru", "example@example", "example@example.com", 
+        "testAdress_911@gmail.com", 
+        "main.example@123yandex.com",
+        "ma-in example@yitmot.com",
+        "one_more-test1@mail..com",
+        "123@yai.com.ru",
+        "test@yai.con..ru"
+]
+
+pattern = r"[a-zA-Z0-9._]+@([a-zA-Z.]+(\.[a-zA-Z.]+)+)$"
 
 
 def main():
-    res = re.match(pattern, email3)
-    
-    if res:
-        print(res.group())
-    else:
-        print("Fail!")
+    for test in tests:
+        res = re.match(pattern, test)
+        if res:
+            print(f"{test} Почтовый сервис: {res[1]}")
+        else:
+            print(f"{test} Почтовый сервис: Fail!")
+        print()
 
 if __name__ == "__main__":
     main()
---
+++ Inf/Lab3/Informatics_Lab3_Task3.py
@@ -0,0 +1,5 @@
+# Author = Shulai Roman Yurievich
+# Group = P3115
+# Date = 18.10.2025
+
+# 501595 % 3 = 1
