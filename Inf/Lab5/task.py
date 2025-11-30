5494b343736b591f2a73e61f8d2b47062e6d839a
roma-dxunvrs <r.shulai@yandex.ru>
2025-12-01 00:47:34 +0300

    Inf: lab5 dop2 work continue

--- Inf/Lab5/task.py
+++ Inf/Lab5/task.py
@@ -1,15 +1,13 @@
 #  501595 % 2 = 1 => pandas
 import pandas
+
 from openpyxl import load_workbook
 from openpyxl.styles import Border, Side
 
-data_frame = pandas.read_excel("Отчет_информатика_лаба5_Шулай.xlsx",
+data_frame = pandas.read_excel("lab5.xlsx",
                                usecols=[i for i in range(25)],
                                skiprows=2,
                                nrows=12)
 data_frame = data_frame.drop(columns=data_frame.columns[5]) # удаляем столбец F
-
-workbook = load_workbook(data_frame)
-print(workbook)
 
 print(data_frame)
