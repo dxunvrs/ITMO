3fcbe9cfeb1c6a2b656957148462f03408ef6d94
roma-dxunvrs <r.shulai@yandex.ru>
2026-02-20 22:17:06 +0300

    Squashed 'Prog/Lab5/' changes from 8e7f9e8..a5812f7
    a5812f7 New command: execute_script Add ShadowJar to project No debug
    c58a284 Add readme Dependency Injection for commands and CommandManager
    
    git-subtree-dir: Prog/Lab5
    git-subtree-split: a5812f70c502af867b9ab4d67978b14f508eb787

--- src/main/java/io/UserInput.java
+++ src/main/java/io/UserInput.java
@@ -1,5 +1,12 @@
 package io;
 
+import core.CommandRegistry;
+
 public interface UserInput {
     String readNextLine(String prompt);
+    void interactive(CommandRegistry commandManager);
+    void refreshInput();
+    boolean isScriptMode();
+    void addScriptCount();
+    void subScriptCount();
 }
