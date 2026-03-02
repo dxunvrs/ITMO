919bedde1c4214e90ecc8a9c6cc761cc4519e284
roma-dxunvrs <r.shulai@yandex.ru>
2026-03-02 23:30:16 +0300

    DB: lab1 updated and fixed

--- DB/Lab1/create_script.sql
+++ DB/Lab1/create_script.sql
@@ -1,62 +1,89 @@
 BEGIN;
 DROP TABLE IF EXISTS university CASCADE;
 DROP TABLE IF EXISTS scientist CASCADE;
-DROP TABLE IF EXISTS dna CASCADE;
-DROP TABLE IF EXISTS animal CASCADE;
-DROP TABLE IF EXISTS fossil CASCADE;
+DROP TABLE IF EXISTS university_workers CASCADE;
 DROP TABLE IF EXISTS fossils_research CASCADE;
-DROP TABLE IF EXISTS hypothesis CASCADE;
-DROP TABLE IF EXISTS hypotheses_research CASCADE;
+DROP TABLE IF EXISTS scientists_in_research CASCADE;
+DROP TABLE IF EXISTS animal CASCADE;
+DROP TABLE IF EXISTS dna_sample CASCADE;
+DROP TABLE IF EXISTS dna_in_research CASCADE;
+DROP TABLE IF EXISTS theory CASCADE;
+DROP TABLE IF EXISTS scientists_proofs CASCADE;
 
-CREATE TABLE University (
+CREATE TABLE university (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    full_name TEXT NOT NULL,
+    short_name TEXT NOT NULL,
     country TEXT NOT NULL,
     city TEXT NOT NULL
 );
 
-CREATE TABLE Scientist (
+CREATE TABLE scientist (
     id SERIAL PRIMARY KEY,
     surname TEXT NOT NULL,
     name TEXT NOT NULL,
-    university_id INTEGER NOT NULL REFERENCES university(id)
+    specialization TEXT NOT NULL,
+    birthday DATE NOT NULL
 );
 
-CREATE TABLE DNA (
+CREATE TABLE university_workers (
     id SERIAL PRIMARY KEY,
-    organic_percentage NUMERIC(5,2) CHECK(organic_percentage >= 0 AND organic_percentage <= 100)
+    scientist_id INTEGER NOT NULL REFERENCES scientist(id),
+    university_id INTEGER NOT NULL REFERENCES university(id),
+    start_date DATE NOT NULL,
+    end_date DATE
 );
 
-CREATE TABLE Animal (
+CREATE TABLE fossils_research (
+    id SERIAL PRIMARY KEY,
+    fossils_age INTEGER NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    place TEXT NOT NULL
+);
+
+CREATE TABLE scientists_in_research (
+    id SERIAL PRIMARY KEY,
+    scientist_id INTEGER NOT NULL REFERENCES scientist(id),
+    fossils_research_id INTEGER NOT NULL REFERENCES fossils_research(id),
+    start_date DATE NOT NULL,
+    end_date DATE
+);
+
+CREATE TABLE animal (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     era TEXT NOT NULL,
-    extinction BOOLEAN NOT NULL,
-    dna_id INTEGER REFERENCES dna(id)
+    extinction BOOLEAN NOT NULL
 );
 
-CREATE TABLE Fossil (
+CREATE TABLE dna_sample (
     id SERIAL PRIMARY KEY,
-    place TEXT NOT NULL,
-    age INTEGER NOT NULL,
+    organic_percentage NUMERIC(5,2) CHECK (organic_percentage >= 0 AND organic_percentage <= 100),
+    sex CHAR(1) NOT NULL CHECK (sex IN ('M', 'F')),
+    animal_age INTEGER NOT NULL,
     animal_id INTEGER NOT NULL REFERENCES animal(id)
 );
 
-CREATE TABLE Fossils_research (
+CREATE TABLE dna_in_research (
     id SERIAL PRIMARY KEY,
-    scientist_id INTEGER NOT NULL REFERENCES scientist(id),
-    fossil_id INTEGER NOT NULL REFERENCES fossil(id)
+    fossils_research_id INTEGER NOT NULL REFERENCES fossils_research(id),
+    dna_id INTEGER NOT NULL REFERENCES dna_sample(id)
 );
 
-CREATE TABLE Hypothesis (
+CREATE TABLE theory (
     id SERIAL PRIMARY KEY,
-    animal_id INTEGER NOT NULL REFERENCES animal(id),
-    proof BOOLEAN NOT NULL
+    description TEXT NOT NULL,
+    proof BOOLEAN NOT NULL,
+    animal_id INTEGER NOT NULL REFERENCES animal(id)
+
 );
 
-CREATE TABLE Hypotheses_research (
+CREATE TABLE scientists_proofs (
     id SERIAL PRIMARY KEY,
     scientist_id INTEGER NOT NULL REFERENCES scientist(id),
-    hypothesis_id INTEGER NOT NULL REFERENCES hypothesis(id)
+    theory_id INTEGER NOT NULL REFERENCES theory(id),
+    result_of_research_id INTEGER NOT NULL REFERENCES fossils_research(id),
+    date DATE NOT NULL
 );
 COMMIT;
--- DB/Lab1/values_script.sql
+++ DB/Lab1/values_script.sql
@@ -1,71 +1,93 @@
 BEGIN;
-INSERT INTO university (name, country, city) 
-VALUES ('UC Berkeley', 'USA', 'Berkeley');
-INSERT INTO university (name, country, city)
-VALUES ('Keio University', 'Japan', 'Tokyo');
-INSERT INTO university (name, country, city)
-VALUES ('Imperial College London', 'UK', 'London');
+INSERT INTO university (full_name, short_name, country, city)
+VALUES ('University of California, Berkeley', 'Berkeley', 'USA', 'Berkeley');
+INSERT INTO university (full_name, short_name, country, city)
+VALUES ('Keio University', 'Keio', 'Japan', 'Tokyo');
+INSERT INTO university (full_name, short_name, country, city)
+VALUES ('The Imperial College of Science, Technology and Medicine', 'Imperial College', 'UK', 'London');
+INSERT INTO university (full_name, short_name, country, city)
+VALUES ('Harvard University', 'Harvard', 'USA', 'Cambridge');
 
-INSERT INTO scientist (surname, name, university_id)
-VALUES ('Marshall', 'Charles', 1);
-INSERT INTO scientist (surname, name, university_id)
-VALUES ('Suwa', 'Genya', 2);
-INSERT INTO scientist (surname, name, university_id)
-VALUES ('Barrett', 'Paul', 3);
+INSERT INTO scientist (surname, name, specialization, birthday)
+VALUES ('Marshall', 'Charles', 'Paleobiology', '1961-01-02');
+INSERT INTO scientist (surname, name, specialization, birthday)
+VALUES ('Suwa', 'Genya', 'Paleoanthropology', '1954-02-12');
+INSERT INTO scientist (surname, name, specialization, birthday)
+VALUES ('Barrett', 'Paul', 'Palaeobiology', '21-09-1971');
 
-INSERT INTO dna (organic_percentage)
-VALUES (50);
-INSERT INTO dna (organic_percentage)
-VALUES (75);
-INSERT INTO dna (organic_percentage)
-VALUES (20);
+INSERT INTO university_workers (scientist_id, university_id, start_date, end_date)
+VALUES (1, 1, '2010-07-01', NULL);
+INSERT INTO university_workers (scientist_id, university_id, start_date, end_date)
+VALUES (2, 2, '1990-06-30', NULL);
+INSERT INTO university_workers (scientist_id, university_id, start_date, end_date)
+VALUES (3, 3, '2003-08-20', NULL);
+INSERT INTO university_workers (scientist_id, university_id, start_date, end_date)
+VALUES (1, 4, '1999-07-01', '2009-06-30');
 
-INSERT INTO animal (name, era, extinction, dna_id)
-VALUES ('Turtle', 'Paleogen', FALSE, 1);
-INSERT INTO animal (name, era, extinction, dna_id)
-VALUES ('Ramidus', 'Miotsen', TRUE, 2);
-INSERT INTO animal (name, era, extinction, dna_id)
-VALUES ('Placodermi', 'Devon', TRUE, 3);
+INSERT INTO fossils_research (fossils_age, start_date, end_date, place)
+VALUES (30_000_000, '2012-01-01', '2020-01-01', 'Montana');
+INSERT INTO fossils_research (fossils_age, start_date, end_date, place)
+VALUES (5_000_000, '2000-01-01', '2015-01-01', 'Ethiopia');
+INSERT INTO fossils_research (fossils_age, start_date, end_date, place)
+VALUES (380_000_000, '2005-01-1', '2018-01-01', 'Australia');
 
-INSERT INTO fossil (place, age, animal_id)
-VALUES ('Montana', 30000000, 1);
-INSERT INTO fossil (place, age, animal_id)
-VALUES ('Ethiopia', 5000000, 2);
-INSERT INTO fossil (place, age, animal_id)
-VALUES ('Australia', 380000000, 3);
+INSERT INTO scientists_in_research (scientist_id, fossils_research_id, start_date, end_date)
+VALUES (1, 1, '2012-01-01', '2019-12-20');
+INSERT INTO scientists_in_research (scientist_id, fossils_research_id, start_date, end_date)
+VALUES (2, 2, '2010-01-01', '2015-01-01');
+INSERT INTO scientists_in_research (scientist_id, fossils_research_id, start_date, end_date)
+VALUES (3, 3, '2005-01-01', '2018-01-01');
+INSERT INTO scientists_in_research (scientist_id, fossils_research_id, start_date, end_date)
+VALUES (1, 2, '2001-01-01', '2014-12-20');
+INSERT INTO scientists_in_research (scientist_id, fossils_research_id, start_date, end_date)
+VALUES (2, 3, '2006-01-01', '2007-12-20');
+INSERT INTO scientists_in_research (scientist_id, fossils_research_id, start_date, end_date)
+VALUES (3, 1, '2012-01-01', '2016-12-20');
 
-INSERT INTO fossils_research (scientist_id, fossil_id)
+INSERT INTO animal (name, era, extinction) 
+VALUES ('Turtle', 'Paleogen', FALSE);
+INSERT INTO animal (name, era, extinction) 
+VALUES ('Ramidus', 'Miotsen', TRUE);
+INSERT INTO animal (name, era, extinction) 
+VALUES ('Placodermi', 'Devon', TRUE);
+
+INSERT INTO dna_sample (organic_percentage, sex, animal_age, animal_id)
+VALUES (50.32, 'M', 56, 1);
+INSERT INTO dna_sample (organic_percentage, sex, animal_age, animal_id)
+VALUES (60.43, 'F', 25, 1);
+INSERT INTO dna_sample (organic_percentage, sex, animal_age, animal_id)
+VALUES (75.66, 'M', 87, 2);
+INSERT INTO dna_sample (organic_percentage, sex, animal_age, animal_id)
+VALUES (75.8, 'M', 45, 2);
+INSERT INTO dna_sample (organic_percentage, sex, animal_age, animal_id)
+VALUES (14.23, 'F', 120, 3);
+
+INSERT INTO dna_in_research (fossils_research_id, dna_id)
 VALUES (1, 1);
-INSERT INTO fossils_research (scientist_id, fossil_id)
-VALUES (2, 2);
-INSERT INTO fossils_research (scientist_id, fossil_id)
-VALUES (3, 3);
-INSERT INTO fossils_research (scientist_id, fossil_id)
+INSERT INTO dna_in_research (fossils_research_id, dna_id)
 VALUES (1, 2);
-INSERT INTO fossils_research (scientist_id, fossil_id)
+INSERT INTO dna_in_research (fossils_research_id, dna_id)
 VALUES (2, 3);
-INSERT INTO fossils_research (scientist_id, fossil_id)
-VALUES (3, 1);
+INSERT INTO dna_in_research (fossils_research_id, dna_id)
+VALUES (2, 4);
+INSERT INTO dna_in_research (fossils_research_id, dna_id)
+VALUES (3, 5);
 
-INSERT INTO hypothesis (animal_id, proof)
-VALUES (1, TRUE);
-INSERT INTO hypothesis (animal_id, proof)
-VALUES (1, FALSE);
-INSERT INTO hypothesis (animal_id, proof)
-VALUES (2, FALSE);
-INSERT INTO hypothesis (animal_id, proof)
-VALUES (3, FALSE);
+INSERT INTO theory (description, proof, animal_id)
+VALUES ('I have been studying the remains of Master Oogway', FALSE, 1);
+INSERT INTO theory (description, proof, animal_id)
+VALUES ('There are turtles today too', TRUE, 1);
+INSERT INTO theory (description, proof, animal_id)
+VALUES ('Ramidus the ancient ancestors of man', TRUE, 2);
+INSERT INTO theory (description, proof, animal_id)
+VALUES ('Placoderms became extinct due to the Ice Age', TRUE, 3);
 
-INSERT INTO hypotheses_research (scientist_id, hypothesis_id)
-VALUES (1, 1);
-INSERT INTO hypotheses_research (scientist_id, hypothesis_id)
-VALUES (1, 2);
-INSERT INTO hypotheses_research (scientist_id, hypothesis_id)
-VALUES (2, 2);
-INSERT INTO hypotheses_research (scientist_id, hypothesis_id)
-VALUES (2, 3);
-INSERT INTO hypotheses_research (scientist_id, hypothesis_id)
-VALUES (3, 3);
-INSERT INTO hypotheses_research (scientist_id, hypothesis_id)
-VALUES (3, 1);
+INSERT INTO scientists_proofs (scientist_id, theory_id, result_of_research_id, date)
+VALUES (1, 1, 1, '2020-02-01');
+INSERT INTO scientists_proofs (scientist_id, theory_id, result_of_research_id, date)
+VALUES (3, 2, 1, '2017-01-30');
+INSERT INTO scientists_proofs (scientist_id, theory_id, result_of_research_id, date)
+VALUES (2, 3, 2, '2015-01-01');
+INSERT INTO scientists_proofs (scientist_id, theory_id, result_of_research_id, date)
+VALUES (3, 4, 3, '2019-01-25');
 COMMIT;
